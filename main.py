6c78ebc02bf8e4f30c00abe701d5006804d51291
Saswat Nayak <saswatnayak@Saswats-MacBook-Pro-2.local>
2025-02-11 15:24:29 -0600

    dfvdfv

--- main.py
+++ main.py
@@ -192,7 +192,13 @@ async def save_logs(request: LogRequest):
 
 
 @app.post("/check-user")
-async def check_user(candidate: Candidate):
+def check_user(candidate: Candidate):
+    LIBSQL_URL = "libsqls://saswat-sash.turso.io"
+    load_dotenv()
+    LIBSQL_AUTH_TOKEN = os.getenv("LIBSQL_AUTH_TOKEN") 
+    conn = libsql.connect("saswat",sync_url=LIBSQL_URL, auth_token=LIBSQL_AUTH_TOKEN)
+    cursor = conn.cursor()
+
     print(f"🔍 Checking user: {candidate.name}")
 
     try:
